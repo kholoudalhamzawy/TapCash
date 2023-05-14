3b8fe4c7a8ac011f2a2cc3641d171efb6f672717
KH <63861068+kholoudalhamzawy@users.noreply.github.com>
2023-05-14 04:12:06 +0300

    Update SignupUserRequest.swift

--- Model/SignupUserRequest.swift
+++ Model/SignupUserRequest.swift
@@ -7,9 +7,30 @@
 
 import Foundation
 
-struct SignupUserRequest: Codable {
+struct SignupUserRequest {
     let email: String
     let name: String
     let password: String
     let phone: String
+    lazy var parameters = [
+      [
+        "key": "name",
+        "value": name,
+        "type": "text"
+      ],
+      [
+        "key": "email",
+        "value": email,
+        "type": "text"
+      ],
+      [
+        "key": "password",
+        "value": password,
+        "type": "text"
+      ],
+      [
+        "key": "phone",
+        "value": phone,
+        "type": "text"
+      ]] as [[String: Any]]
 }
