882fff22d152e8b4b84c2f097a9701739389e5c7
KH <63861068+kholoudalhamzawy@users.noreply.github.com>
2023-05-14 04:11:19 +0300

    Update Response.swift

--- Model/Response.swift
+++ Model/Response.swift
@@ -7,6 +7,33 @@
 
 import Foundation
 
+
+// MARK: - TransferResponse
+struct TransferResponse: Codable {
+    let status: Int
+    let message: String
+    let data: DataTransferClass
+}
+
+// MARK: - DataTransferClass
+struct DataTransferClass: Codable {
+    let amount, balanceBefore, blanceAfter: String
+}
+
+
+// MARK: - BalanceResponse
+struct BalanceResponse: Codable {
+    let status: Int
+    let message: String
+    let data: DataClass
+}
+
+// MARK: - DataClass
+struct DataClass: Codable {
+    let balance: String
+}
+
+
 // MARK: - Response
 struct Response: Codable {
     let status: String
