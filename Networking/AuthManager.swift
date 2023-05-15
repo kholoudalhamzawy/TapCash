902d0fd54fa803ac2089b691f3f9c7d666bd61f2
KH <63861068+kholoudalhamzawy@users.noreply.github.com>
2023-05-15 05:00:48 +0300

    Update AuthManager.swift

--- Networking/AuthManager.swift
+++ Networking/AuthManager.swift
@@ -65,7 +65,11 @@ class AuthManager {
                 let results = try JSONDecoder().decode(Response.self, from: data)
                 completion(.success(results))
             } catch {
-                completion(.failure(ServiceError.decodingError(str)))
+//                if let res = data.result.value {
+                let response = data as! NSDictionary
+                                   let message = response.object(forKey: "message")!
+                                   print(message)
+                completion(.failure(ServiceError.decodingError(message as! String)))
             }
         }
         
@@ -108,3 +112,63 @@ class AuthManager {
     }
     
 }
+//
+//    func fetch(request: URLRequest, completion: @escaping (Result<String, Error>) -> Void) {
+//
+//        URLSession.shared.dataTask(with: request) { data, _, error in
+//            guard let data = data else {
+//                if let error = error {
+//                    completion(.failure(ServiceError.serverError(error.localizedDescription)))
+//                } else {
+//                    completion(.failure(ServiceError.unkown()))
+//                }
+//                return
+//            }
+//
+//            let decoder = JSONDecoder()
+//
+//            if let successMessage =  try? decoder.decode(SucessResponse.self, from: data) {
+//                completion(.success(successMessage.success))
+//                return
+//            }
+//            else if let errorMessage = try? decoder.decode(ErrorResponse.self, from: data) {
+//                completion(.failure(ServiceError.serverError(errorMessage.error)))
+//                return
+//            }
+//            else {
+//                completion(.failure(ServiceError.decodingError()))
+//                return
+//            }
+//        }.resume()
+//    }
+//
+//        // MARK: - Sign Out
+//        static func signOut() {
+//            let url = URL(string: Constants.fullURL)!
+////            let cookie = HTTPCookieStorage.shared.cookies(for: url)!.first!
+//            HTTPCookieStorage.shared.deleteCookie(cookie)
+//        }
+
+
+//
+//    if httpResponse.statusCode == 406 {
+//
+//    }
+//        return URLSession.shared.dataTaskPublisher(for: request)
+//
+//        // tryMap offers a way to process the response with a closure that can throw an error
+//            .tryMap() { element -> Data in
+//                guard let httpResponse = element.response as? HTTPURLResponse else {
+//                    throw ServiceError.decodingError()
+//                }
+//               else {
+//                        throw URLError(.badServerResponse)
+//                    }
+//                return element.data
+//                }
+//                .eraseToAnyPublisher()
+//        // type erase the publisher so that its easy to handle the response in the sink subscriber
+//    }
+//
+//
+//
